928bbc2ec703e2545e7359f2696072bbb72c8f0d
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-04-02 05:14:02 +0300

    basename is added

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import configureStore from './redux/stores/configureStore'
 import App from './App'
-// import Forecast from './components/Forecast/index'
 
 const store = configureStore({})
 
 ReactDOM.render(
   <Provider  store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='forecast-app'>
       <App />
     </BrowserRouter>
   </Provider>,    
