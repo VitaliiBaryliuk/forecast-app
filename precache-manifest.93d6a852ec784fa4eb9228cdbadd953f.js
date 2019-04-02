de74a5f67ef4a932d0f7d4b45a162b8685c3af51
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-04-02 07:21:37 +0300

    deployed by create-react-app

---
+++ precache-manifest.93d6a852ec784fa4eb9228cdbadd953f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d19dac0ad449ca7b8c210ac6580f48a",
+    "url": "/forecast-app/static/media/background.7d19dac0.jpg"
+  },
+  {
+    "revision": "1b8ff24b22f74ad27fb6",
+    "url": "/forecast-app/static/js/runtime~main.cfa6f48a.js"
+  },
+  {
+    "revision": "eb0caad720445c28fed5",
+    "url": "/forecast-app/static/js/main.e39071e9.chunk.js"
+  },
+  {
+    "revision": "e941f3c39fa9845f13c9",
+    "url": "/forecast-app/static/js/2.ad3a9092.chunk.js"
+  },
+  {
+    "revision": "eb0caad720445c28fed5",
+    "url": "/forecast-app/static/css/main.34e04d9b.chunk.css"
+  },
+  {
+    "revision": "918bbf5ae3bbb676f390a02afe364af5",
+    "url": "/forecast-app/index.html"
+  }
+];
